3f3c32ce8e02c001674bfc7d77a28dfde80d2bd9
astek <jeremy.brunel@bashroom.com>
2024-05-26 21:39:06 +0200

    feat: manage error, search stop and css

--- src/BusHoraires.js
+++ src/BusHoraires.js
@@ -1,41 +1,55 @@
 import React, { useState, useEffect } from "react";
 
 const BusHoraires = () => {
-  const [horaires, setHoraires] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [horaires, setHoraires] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [stopCode, setStopCode] = useState("SEM:2273");
+  const [error, setError] = useState(null);
+
+  const fetchHoraires = async () => {
+    setLoading(true);
+    setError(null);
+    setHoraires({});
+    try {
+      const response = await fetch(
+        `https://data.mobilites-m.fr/api/routers/default/index/stops/${stopCode}/stoptimes`
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+
+      const secondsToTime = (seconds) => {
+        const date = new Date(0);
+        date.setSeconds(seconds);
+        return date.toISOString().substr(11, 8);
+      };
+
+      // Regrouper les horaires par terminus
+      const groupedHoraires = data.reduce((acc, pattern) => {
+        const terminus = pattern.pattern.lastStopName;
+        const horairesList = pattern.times.map((time_info) => ({
+          stopName: time_info.stopName,
+          busDesc: pattern.pattern.desc,
+          arrivalTime: secondsToTime(time_info.realtimeArrival),
+        }));
+        if (!acc[terminus]) {
+          acc[terminus] = [];
+        }
+        acc[terminus] = acc[terminus].concat(horairesList);
+        return acc;
+      }, {});
+
+      setHoraires(groupedHoraires);
+    } catch (error) {
+      console.error("Erreur lors de la récupération des horaires :", error);
+      setError("Aucun arrêt ne semble correspondre à ce code");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const secondsToTime = (seconds) => {
-      const date = new Date(0);
-      date.setSeconds(seconds);
-      return date.toISOString().substr(11, 8);
-    };
-
-    const fetchHoraires = async () => {
-      try {
-        const response = await fetch(
-          "https://data.mobilites-m.fr/api/routers/default/index/stops/SEM:2273/stoptimes"
-        );
-        const data = await response.json();
-
-        const horairesData = data
-          .map((pattern) => {
-            return pattern.times.map((time_info) => ({
-              stopName: time_info.stopName,
-              busDesc: pattern.pattern.desc,
-              arrivalTime: secondsToTime(time_info.realtimeArrival),
-            }));
-          })
-          .flat();
-
-        setHoraires(horairesData);
-      } catch (error) {
-        console.error("Erreur lors de la récupération des horaires :", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
     fetchHoraires();
   }, []);
 
@@ -47,16 +61,32 @@ const BusHoraires = () => {
       </h1>
       {loading ? (
         <p>Chargement...</p>
+      ) : error ? (
+        <p>{error}</p>
       ) : (
-        <ul>
-          {horaires.map((horaire, index) => (
-            <li key={index}>
-              Prochain passage à l'arrêt {horaire.stopName} ({horaire.busDesc})
-              : {horaire.arrivalTime}
-            </li>
-          ))}
-        </ul>
+        Object.keys(horaires).map((terminus, index) => (
+          <div key={index}>
+            <h2 className="terminus">Terminus: {terminus}</h2>
+            <hr />
+            <ul>
+              {horaires[terminus].map((horaire, i) => (
+                <li key={i}>{horaire.arrivalTime}</li>
+              ))}
+            </ul>
+            <hr />
+          </div>
+        ))
       )}
+      <div className="input-container">
+        <label htmlFor="stopCode">Code de l'arrêt: </label>
+        <input
+          type="text"
+          id="stopCode"
+          value={stopCode}
+          onChange={(e) => setStopCode(e.target.value)}
+        />
+        <button onClick={fetchHoraires}>Chercher les horaires</button>
+      </div>
       <footer>
         Fait pour Julie{" "}
         <span role="img" aria-label="fleur">
